bc276adf6bc34bcbca2afa32279f3082a1fa1414
Mohit Agrawal <mohitagrawal939@gmail.com>
2025-01-06 01:33:29 +0530

    Added english language support

--- src/translateType.ts
+++ src/translateType.ts
@@ -1,4 +1,4 @@
-export type Language = 'japanese' | 'spanish' | 'chinese' | 'hindi';
+export type Language = 'japanese' | 'spanish' | 'chinese' | 'hindi' | 'english';
 
 export type LanguageMap = {
   [key in Language]: { [key: string]: string };
